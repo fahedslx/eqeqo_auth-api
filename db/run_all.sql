7d68fd382932176cb46fed11528420a7eff45170
fahedsl <fahedsl@yandex.com>
2025-10-10 07:30:45 -0500

    Fix list functions and clarify DB setup output

--- db/run_all.sql
+++ db/run_all.sql
@@ -1,12 +1,27 @@
 -- This script executes all other SQL scripts in the correct order.
 -- It's recommended to run this file to set up the database from scratch.
 
+\set ON_ERROR_STOP on
+
+\echo 'Connecting to postgres database...'
 \c postgres;
+
+\echo 'Recreating auth_db database...'
 DROP DATABASE IF EXISTS auth_db;
 CREATE DATABASE auth_db;
+
+\echo 'Switching connection to auth_db...'
 \c auth_db;
 
-\i structure.sql
-\i procedures.sql
+\echo 'Loading database structure...'
+\ir structure.sql
+
+\echo 'Loading stored procedures...'
+\ir procedures.sql
+
+-- Uncomment the next line if you also want to load the demo data.
+-- \ir demo_data.sql
+
+\echo 'Database setup completed successfully.'
 
 -- End of script
